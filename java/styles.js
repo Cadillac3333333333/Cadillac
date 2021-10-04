11a63c57e7a9ee0904f1e3d2149dd9727d4a07c3
Cadillac3333333333 <90277591+Cadillac3333333333@users.noreply.github.com>
2021-10-04 13:59:19 -0400

    Update styles.js

--- java/styles.js
+++ java/styles.js
@@ -1,1 +1,14 @@
-
+var a = parseInt(prompt("Combien de boulons voulez-vous? 5¢"));
+var b = parseInt(prompt("Combien de rondelles voulez-vous? 1¢"));
+var c = parseInt(prompt("Combien d'écrous voulez-vous? 3¢ "));
+if (b == c && c == a){
+  var tax = 1.13
+var a = a*5
+var b = b*1
+var c = c*3
+  var prixtotal = (a+b+c*tax)/100
+var prix = Math.round(prixtotal * 100) / 100
+alert("Le coût est " + prix + " $")
+} else {
+  alert("Regarde a ton ordre encore! Tu veux avoir le même nombre de boulons et d'écrous!")
+}
